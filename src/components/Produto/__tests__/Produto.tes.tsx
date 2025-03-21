ef167f2b94d0531b883dd82229c4f1c914a8f267
MitsukiKz <tailorkunz@gmail.com>
2025-03-21 17:06:38 -0300

    finalizando aula

---
+++ src/components/Produto/__tests__/Produto.tes.tsx
@@ -0,0 +1,27 @@
+import { fireEvent, screen } from '@testing-library/react'
+import Produto from '..'
+import { renderizaComProvider } from '../../../utils/tests'
+
+const jogo = {
+  id: 2,
+  categoria: 'FPS',
+  imagem: '',
+  plataformas: ['Windows', 'PS5', 'Xbox Series S/X'],
+  preco: 199.9,
+  precoAntigo: 299.9,
+  titulo: 'Valorant'
+}
+
+describe('Testes para o componente produto', () => {
+  test('Deve renderizar corretamente', () => {
+    renderizaComProvider(<Produto game={jogo} />)
+    expect(screen.getByText('Valorant')).toBeInTheDocument()
+  })
+  test('Deve adicionar um item ao carrinho', () => {
+    const { store } = renderizaComProvider(<Produto game={jogo} />)
+    const botao = screen.getByTestId('btn-adicionar-produto')
+    fireEvent.click(botao)
+    store.getState().carrinho.itens
+    expect(store.getState().carrinho.itens).toHaveLength(1)
+  })
+})
---
+++ src/containers/Produtos/__tests__/Produtos.test.tsx
@@ -0,0 +1,70 @@
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import Produtos from '..'
+import { renderizaComProvider } from '../../../utils/tests'
+import { screen, waitFor } from '@testing-library/react'
+
+const mocks = [
+  {
+    id: 1,
+    categoria: 'RPG',
+    imagem: '',
+    plataformas: ['Windows'],
+    preco: 150.9,
+    precoAntigo: 199.9,
+    titulo: 'Elden Ring'
+  },
+  {
+    id: 2,
+    categoria: 'FPS',
+    imagem: '',
+    plataformas: ['Windows', 'PS5', 'Xbox Series S/X'],
+    preco: 199.9,
+    precoAntigo: 299.9,
+    titulo: 'Valorant'
+  },
+  {
+    id: 3,
+    categoria: 'AÇÃO',
+    imagem: '',
+    plataformas: ['PS5', 'Xbox Series S/X'],
+    preco: 150,
+    precoAntigo: 200,
+    titulo: 'Gotham Knights'
+  },
+  {
+    id: 4,
+    categoria: 'Aventura',
+    imagem: '',
+    plataformas: ['Nintendo Switch'],
+    preco: 189.9,
+    precoAntigo: 299.9,
+    titulo: 'Donkey Kong'
+  }
+]
+const server = setupServer(
+  rest.get(
+    'http://localhost:4000/produtos',
+    (requisicao, resposta, contexto) => {
+      return resposta(contexto.json(mocks))
+    }
+  )
+)
+
+describe('Teste para o container produtos', () => {
+  beforeAll(() => server.listen())
+  afterEach(() => server.restoreHandlers())
+  afterAll(() => server.close())
+
+  test('Deve renderizar corretamente com o texto de carrregamento', () => {
+    renderizaComProvider(<Produtos />)
+    expect(screen.getByText('Carregando...')).toBeInTheDocument()
+  })
+
+  test('Deve renderizar corretamente com a listagem de jogos', async () => {
+    renderizaComProvider(<Produtos />)
+    await waitFor(() => {
+      expect(screen.getByText('Donkey Kong')).toBeInTheDocument()
+    })
+  })
+})
